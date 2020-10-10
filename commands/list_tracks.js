3303fc515ff0944d5867dc9aa7da03ad468a9a8b
Bogdan Bodnarescu <bbogdanmircea@gmail.com>
2020-10-10 13:19:09 +0200

    add comments to understand code

--- commands/list_tracks.js
+++ commands/list_tracks.js
@@ -6,23 +6,24 @@ module.exports = {
   aliases: ["tracks"],
   description: "Display all tracks available on server",
   execute(message) {
-    let tracks = AcUtil.getTracksAvailableOnServer();
-    
+    let tracks = AcUtil.getTracksAvailableOnServer(); //get all tracks form the server in tracks
+    // create tracksEmbed for the Discord Message
     let tracksEmbed = new MessageEmbed()
+	// header of the message
     .setTitle("Server tracks")
     .setDescription("List of all server tracks")
     .setColor("#F8AA2A");
-    
+    // take each track inside tracks
     tracks.forEach((track) => {
       tracksEmbed.addField(
         `**${track.name}**`,
-        `${track.configs.join(', ')}`,
+        `${track.configs && track.configs.length ? track.configs.join(', ') : '(no configs detected), use empty string'}`,
         true
         );
       });
-      
+      // add a timestamp to the message
       tracksEmbed.setTimestamp();
-      
+      // send tracksEmbed
       return message.channel.send(tracksEmbed).catch(console.error);
     }
   };
