e60a13d15a78a8764964defcc1576833496b38d9
bbogdanmircea <bbogdanmircea@gmail.com>
2020-10-14 15:22:48 +0200

    add commets to understand code

--- commands/get_server_status.js
+++ commands/get_server_status.js
@@ -3,26 +3,26 @@ const AcUtil = require('../util/AcUtil');
 
 module.exports = {
   name: "get_server_status",
-  aliases: ["server_status"],
+  aliases: ["status"],
   description: "Display server status",
   execute(message) {
-    let configurations = AcUtil.getCompleteConfiguration();
-    let status = AcUtil.getServerStatus();
+    let configurations = AcUtil.getCompleteConfiguration(); // get server configuration
+    let status = AcUtil.getServerStatus(); // get server status
     
-    let configurationsEmbed = new MessageEmbed()
+    let configurationsEmbed = new MessageEmbed() // create message for Discord
     .setTitle("Server configurations")
     .setDescription("List of all server configurations")
     .setColor("#F8AA2A");
 
-    configurationsEmbed.addField('Server name:', `${configurations.SERVER.NAME}`, true);
-    configurationsEmbed.addField('Status:', `${status}`, true);
+    configurationsEmbed.addField('Server name:', `${configurations.SERVER.NAME}`, true); // add server name to message
+    configurationsEmbed.addField('Status:', `${status}`, true); // add server status to message
 
-    configurationsEmbed.addField('Track:', `${configurations.SERVER.TRACK} - ${configurations.SERVER.CONFIG_TRACK}`, true);
-    configurationsEmbed.addField('Cars:', configurations.SERVER.CARS, true);
+    configurationsEmbed.addField('Track:', `${configurations.SERVER.TRACK} - ${configurations.SERVER.CONFIG_TRACK}`, true); // add track to message
+    configurationsEmbed.addField('Cars:', configurations.SERVER.CARS, true); // add cars to message
       
-    configurationsEmbed.setTimestamp();
+    configurationsEmbed.setTimestamp(); // add timestamp to message
       
-    return message.channel.send(configurationsEmbed).catch(console.error);
+    return message.channel.send(configurationsEmbed).catch(console.error); // put message in Discord
   }
 };
   
