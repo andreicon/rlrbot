8d5a0b5e08230eff209cc4d3bfd5708a963d51e0
bbogdanmircea <bbogdanmircea@gmail.com>
2020-10-14 15:19:27 +0200

    add comments to understand code

--- commands/help.js
+++ commands/help.js
@@ -5,23 +5,23 @@ module.exports = {
   aliases: ["h"],
   description: "Display all commands and descriptions",
   execute(message) {
-    let commands = message.client.commands.array();
+    let commands = message.client.commands.array(); // get all commands
 
-    let helpEmbed = new MessageEmbed()
+    let helpEmbed = new MessageEmbed() // create message for Discord
       .setTitle("RLRbot Help")
       .setDescription("List of all commands")
       .setColor("#F8AA2A");
 
-    commands.forEach((cmd) => {
-      helpEmbed.addField(
+    commands.forEach((cmd) => { // take each command
+      helpEmbed.addField( // add command info to message
         `**${message.client.prefix}${cmd.name} ${cmd.aliases ? `(${cmd.aliases})` : ""}**`,
         `${cmd.description}`,
         true
       );
     });
 
-    helpEmbed.setTimestamp();
+    helpEmbed.setTimestamp(); // add timestamp to message
 
-    return message.channel.send(helpEmbed).catch(console.error);
+    return message.channel.send(helpEmbed).catch(console.error); // put message to Discord
   }
 };
