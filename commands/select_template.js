7cfdfd984e6c2dd0862d89cafdd6812cfa1dd6e3
bbogdanmircea <bbogdanmircea@gmail.com>
2020-10-14 15:31:16 +0200

    add comments, return error message

--- commands/select_template.js
+++ commands/select_template.js
@@ -7,11 +7,12 @@ module.exports = {
   description: "Choose a template to overwrite server config from",
   usage: "/select_template uuid",
   execute(message, args) {
-    let template = AcUtil.applyTemplatedConfiguration(args[0]);
-    if (template) {
-      return message.channel.send(`Template "${template.name}" applied`).catch(console.error);
+    let template = AcUtil.applyTemplatedConfiguration(args[0]); // apply the template given by uuid
+    if (template) { // if the template was selected successfully
+      return message.channel.send(`Template "${template.name}" applied`).catch(console.error); // return the name of the template
+    } else { // return error message if template does not exist
+      return message.channel.send(`Couldn't select template, dunno`).catch(console.error);
     }
-    return message.channel.send(`Couldn't select template, dunno`).catch(console.error);
   }
 };
   
