58af81d3df627ec4b1f2a649817b1f56b036ed2e
bbogdanmircea <bbogdanmircea@gmail.com>
2020-10-14 15:15:50 +0200

    Update with comments to understand code

--- commands/get_complete_configuration.js
+++ commands/get_complete_configuration.js
@@ -6,21 +6,21 @@ module.exports = {
   aliases: ["complete_config"],
   description: "Display all configuration options",
   execute(message) {
-    let configurations = AcUtil.getCompleteConfiguration();
+    let configurations = AcUtil.getCompleteConfiguration(); // get the complete configuration from AcUtil
     
-    let configurationsEmbed = new MessageEmbed()
+    let configurationsEmbed = new MessageEmbed() // create the message for Discord
     .setTitle("Server configurations")
     .setDescription("List of all server configurations")
     .setColor("#F8AA2A");
 
-    configurationsEmbed.addField('Server name:', `${configurations.SERVER.NAME}`, true);
+    configurationsEmbed.addField('Server name:', `${configurations.SERVER.NAME}`, true); // add the server name to the message
 
-    configurationsEmbed.addField('Track:', `${configurations.SERVER.TRACK} - ${configurations.SERVER.CONFIG_TRACK}`, true);
-    configurationsEmbed.addField('Cars:', configurations.SERVER.CARS, true);
+    configurationsEmbed.addField('Track:', `${configurations.SERVER.TRACK} - ${configurations.SERVER.CONFIG_TRACK}`, true); // add the track to the message
+    configurationsEmbed.addField('Cars:', configurations.SERVER.CARS, true); // add the cars to the message
       
-    configurationsEmbed.setTimestamp();
+    configurationsEmbed.setTimestamp(); // add the timestamp to the message
       
-    return message.channel.send(configurationsEmbed).catch(console.error);
+    return message.channel.send(configurationsEmbed).catch(console.error); // put the message
   }
 };
   
